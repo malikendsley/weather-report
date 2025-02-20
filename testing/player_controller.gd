9384c454240a7b0cfaf7a692e2a5dc5d3f2fdaa6
Malik Endsley <malik.s.endsley@gmail.com>
2025-02-20 01:32:14 -0500

    import project

---
+++ testing/player_controller.gd
@@ -0,0 +1,76 @@
+extends CharacterBody2D
+
+@onready var state_label: Label = $CurStateLabel
+
+const SPEED = 300.0 # px/s
+const ACCELERATION = 1000.0 # px/s^2
+const FRICTION = 2000.0 # px/s^2
+const MAX_FALL_SPEED = 1000.0 # px/s
+const GRAVITY = 2000.0 # px/s^2
+const JUMP_VELOCITY = -400.0
+
+
+enum PlayerState {
+	idle, # Standing still.
+	walk, # Walking.
+	run, # Running.
+	jumping, # Ascending.
+	fall, # Descending.
+	slide, # During a low-dash
+}
+var state_names := {
+	PlayerState.idle: "Free",
+	PlayerState.walk: "Walk",
+	PlayerState.run: "Run",
+	PlayerState.jumping: "Jumping",
+	PlayerState.fall: "Fall",
+	PlayerState.slide: "Slide"
+}
+var current_state := PlayerState.idle:
+	set(new_state):
+		state_label.text = state_names[new_state]
+		current_state = new_state
+
+var input: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	state_label.text = state_names[current_state]
+
+func _process(_delta: float) -> void:
+	input.x = Input.get_axis("move_left", "move_right")
+	print("X Input: %s" % input.x)
+
+func _physics_process(delta: float) -> void:
+	input.x = Input.get_axis("move_left", "move_right")
+
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+
+	# Handle movement
+	match current_state:
+		PlayerState.idle:
+			velocity.x = move_toward(velocity.x, input.x * SPEED, ACCELERATION * delta)
+
+			if input.x != 0:
+				current_state = PlayerState.walk
+			elif not is_on_floor():
+				current_state = PlayerState.fall
+
+		PlayerState.walk:
+			velocity.x = move_toward(velocity.x, input.x * SPEED, ACCELERATION * delta)
+
+			if input.x == 0:
+				current_state = PlayerState.idle
+			elif not is_on_floor():
+				current_state = PlayerState.fall
+
+		PlayerState.fall:
+			if is_on_floor():
+				current_state = PlayerState.idle
+
+		_:
+			pass # Other states remain unchanged for now.
+
+	# Apply movement
+	move_and_slide()
