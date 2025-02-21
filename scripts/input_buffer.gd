a56d037f8a3b730851ee07cb710401e994264f67
Malik Endsley <malik.s.endsley@gmail.com>
2025-02-20 23:04:40 -0500

    initial commit (basic scene, move and jump)

---
+++ scripts/input_buffer.gd
@@ -0,0 +1,69 @@
+# Simple input buffer system
+extends Node
+
+# 9 frames, like in SSBB. Many players are unconsciously used to this buffer window
+const BUFFER_WINDOW := 150
+const LMBDOWN = "lmbdown"
+const LMBUP = "lmbup"
+
+var timestamps: Dictionary
+
+func _ready() -> void:
+	# maintain this even if the game is paused
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	timestamps = {}
+
+# hook into standard input event handling to record timestamps for all key presses
+func _input(event: InputEvent) -> void:
+	if event  is InputEventKey:
+		if event.is_echo() or !event.pressed:
+			return
+		var keycode: Key = event.physical_keycode
+		timestamps[keycode] = Time.get_ticks_msec()
+		print("input_buffer: " + OS.get_keycode_string(keycode) + " received: (" + str(keycode) + ")")
+	elif event is InputEventMouseButton:
+		# check if was press
+		if event.pressed:
+			timestamps[LMBDOWN] = Time.get_ticks_msec()
+		else:
+			timestamps[LMBUP] = Time.get_ticks_msec()
+
+# return true if the action is buffered, false otherwise. Consumes the action if it is buffered
+func consume(action: String, custom_buffer: int = BUFFER_WINDOW) -> bool:
+	for event in InputMap.action_get_events(action):
+		if event is InputEventKey:
+			var keycode: Key = event.physical_keycode
+			print("Checking for keycode " + str(keycode))
+			if timestamps.has(keycode):
+				if Time.get_ticks_msec() - timestamps[keycode] <= custom_buffer:
+					# consume the action
+					debounce(action)
+					return true;
+	return false
+
+# returns true if a mouse click has occurred within the buffer window, false otherwise
+func consume_mouse_down(custom_buffer: int = BUFFER_WINDOW) -> bool:
+	if timestamps.has(LMBDOWN):
+		if Time.get_ticks_msec() - timestamps[LMBDOWN] <= custom_buffer:
+			debounce(LMBDOWN)
+			return true
+	return false
+
+# returns true if a mouse release has occurred within the buffer window, false otherwise
+func consume_mouse_up(custom_buffer: int = BUFFER_WINDOW) -> bool:
+	if timestamps.has(LMBUP):
+		if Time.get_ticks_msec() - timestamps[LMBUP] <= custom_buffer:
+			debounce(LMBUP)
+			return true
+	return false
+
+# reset the timestamp for the given action, so that it can be buffered again
+func debounce(action: String) -> void:
+	if action == LMBDOWN or action == LMBUP:
+		timestamps[action] = -INF
+	else:
+		for event in InputMap.action_get_events(action):
+			if event is InputEventKey:
+				var keycode :Key = event.physical_keycode
+				if timestamps.has(keycode):
+					timestamps[keycode] = -INF
--- testing/player_controller.gd
+++ testing/player_controller.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 @onready var state_label: Label = $CurStateLabel
 
 const SPEED = 300.0 # px/s
-const ACCELERATION = 1000.0 # px/s^2
+const ACCELERATION = 3000.0 # px/s^2
 const FRICTION = 2000.0 # px/s^2
 const MAX_FALL_SPEED = 1000.0 # px/s
 const GRAVITY = 2000.0 # px/s^2
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -400.0	
 
 
 enum PlayerState {
@@ -18,6 +18,28 @@ enum PlayerState {
 	fall, # Descending.
 	slide, # During a low-dash
 }
+enum Grenades {
+	stasis,
+	blast,
+}
+
+# TODO: Extract to a persistent state tracker for the player
+var has_dash_slide := false
+var has_jump_cancel := false
+var has_alt_fire := false
+var has_warp_core := false
+var has_stasis_grenade := false
+var has_blast_grenade := false
+
+var grenade_timer := 0.0
+var current_grenade := Grenades.stasis
+var grenade_deployed := false
+
+var warp_core_timer := 0.0
+var warp_core_deployed:= false
+
+var air_dashes_left := 0
+
 var state_names := {
 	PlayerState.idle: "Free",
 	PlayerState.walk: "Walk",
@@ -31,17 +53,20 @@ var current_state := PlayerState.idle:
 		state_label.text = state_names[new_state]
 		current_state = new_state
 
-var input: Vector2 = Vector2.ZERO
+var directional_input := Vector2.ZERO
+
+
+
+
 
 func _ready() -> void:
 	state_label.text = state_names[current_state]
 
 func _process(_delta: float) -> void:
-	input.x = Input.get_axis("move_left", "move_right")
-	print("X Input: %s" % input.x)
+	directional_input.x = Input.get_axis("move_left", "move_right")
+	directional_input.y = Input.get_axis("move_up", "move_down")
 
 func _physics_process(delta: float) -> void:
-	input.x = Input.get_axis("move_left", "move_right")
 
 	# Apply gravity
 	if not is_on_floor():
@@ -50,17 +75,22 @@ func _physics_process(delta: float) -> void:
 	# Handle movement
 	match current_state:
 		PlayerState.idle:
-			velocity.x = move_toward(velocity.x, input.x * SPEED, ACCELERATION * delta)
+			
+			velocity.x = move_toward(velocity.x, directional_input.x * SPEED, ACCELERATION * delta)
 
-			if input.x != 0:
+			if InputBuffer.consume("jump"):
+				print("Jumping")
+				velocity.y = JUMP_VELOCITY
+				current_state = PlayerState.jumping
+			if directional_input.x != 0:
 				current_state = PlayerState.walk
 			elif not is_on_floor():
 				current_state = PlayerState.fall
 
 		PlayerState.walk:
-			velocity.x = move_toward(velocity.x, input.x * SPEED, ACCELERATION * delta)
+			velocity.x = move_toward(velocity.x, directional_input.x * SPEED, ACCELERATION * delta)
 
-			if input.x == 0:
+			if directional_input.x == 0:
 				current_state = PlayerState.idle
 			elif not is_on_floor():
 				current_state = PlayerState.fall
@@ -69,6 +99,13 @@ func _physics_process(delta: float) -> void:
 			if is_on_floor():
 				current_state = PlayerState.idle
 
+		PlayerState.jumping: 
+			if is_on_floor():
+				current_state = PlayerState.idle
+			elif velocity.y < 0:
+				current_state = PlayerState.jumping
+			else:
+				current_state = PlayerState.fall
 		_:
 			pass # Other states remain unchanged for now.
 
